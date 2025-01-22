026e52982be12106445110400dfcf34365b791b0
Zsolt Parragi <zsolt.parragi@percona.com>
2025-01-22 17:32:24 +0000

    Fixing issues with running installcheck-world with pg_tde (#31)
    * The make CI action now also runs the entire installcheck-world
      with pg_tde setup for all tests
    * The meson CI runner doesn't do this yet
    * Tools that only worked with the heap am based on an OID check now
      also check for the tde_heap OID
    * The get_tde_table_am_oid helper function is now moved inside the core,
      as it is required by other contrib modules, which do not have access
      to the tde code otherwise.
    * A few tests that do a custom server setup was disabled based on the
      TDE_MODE environment variable. These tests would fail because they
      expect that after an initdb and start, the regression suite works,
      but that's not the case with tde_heap. These tests can be re-enabled
      again after we have options to do this with initdb

---
+++ ci_scripts/make-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TDE_MODE=1
+
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+INSTALL_DIR="$SCRIPT_DIR/../../pginst"
+
+cd "$SCRIPT_DIR/../"
+
+make check-world
