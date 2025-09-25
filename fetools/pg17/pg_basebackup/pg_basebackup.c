bdf6e62266bd606b9b47676e61610bbd7ca7959e
Andreas Karlsson <andreas.karlsson@percona.com>
2025-09-25 16:00:43 +0200

    PG-1879 Copy frontend code from TDE_REL_17_STABLE
    These are the changes we made to the frontend tools in the Percona
    PostgreSQL repo to make the tools work with pg_tde. The tools will now
    end up as forked copies.
    
    Commit cdfaf2dc751c4424f97e904b13f36836c5377f0c was used for this.

--- fetools/pg17/pg_basebackup/pg_basebackup.c
+++ fetools/pg17/pg_basebackup/pg_basebackup.c
@@ -38,7 +38,18 @@
 #include "receivelog.h"
 #include "streamutil.h"
 
-#define ERRCODE_DATA_CORRUPTED	"XX001"
+#ifdef PERCONA_EXT
+#include "access/pg_tde_fe_init.h"
+#include "access/pg_tde_xlog_smgr.h"
+#include "access/pg_tde_xlog_keys.h"
+#include "access/xlog_smgr.h"
+#include "catalog/tde_principal_key.h"
+#include "pg_tde.h"
+
+#define GLOBAL_DATA_TDE_OID 1664
+#endif
+
+#define ERRCODE_DATA_CORRUPTED_BCP	"XX001"
 
 typedef struct TablespaceListCell
 {
@@ -138,6 +149,7 @@ static bool showprogress = false;
 static bool estimatesize = true;
 static int	verbose = 0;
 static IncludeWal includewal = STREAM_WAL;
+static bool encrypt_wal = false;
 static bool fastcheckpoint = false;
 static bool writerecoveryconf = false;
 static bool do_sync = true;
@@ -409,6 +421,9 @@ usage(void)
 	printf(_("      --waldir=WALDIR    location for the write-ahead log directory\n"));
 	printf(_("  -X, --wal-method=none|fetch|stream\n"
 			 "                         include required WAL files with specified method\n"));
+#ifdef PERCONA_EXT
+	printf(_("  -E, --encrypt-wal      encrypt streamed WAL\n"));
+#endif
 	printf(_("  -z, --gzip             compress tar output\n"));
 	printf(_("  -Z, --compress=[{client|server}-]METHOD[:DETAIL]\n"
 			 "                         compress on client or server as specified\n"));
@@ -561,6 +576,7 @@ LogStreamerMain(logstreamer_param *param)
 	stream.synchronous = false;
 	/* fsync happens at the end of pg_basebackup for all data */
 	stream.do_sync = false;
+	stream.encrypt = encrypt_wal;
 	stream.mark_done = true;
 	stream.partial_suffix = NULL;
 	stream.replication_slot = replication_slot;
@@ -653,6 +669,27 @@ StartLogStreamer(char *startpos, uint32 timeline, char *sysidentifier,
 			 basedir,
 			 PQserverVersion(conn) < MINIMUM_VERSION_FOR_PG_WAL ?
 			 "pg_xlog" : "pg_wal");
+
+#ifdef PERCONA_EXT
+	if (encrypt_wal) {
+		char tdedir[MAXPGPATH];
+		TDEPrincipalKey *principalKey;
+
+		snprintf(tdedir, sizeof(tdedir), "%s/%s", basedir, PG_TDE_DATA_DIR);
+		pg_tde_fe_init(tdedir);
+		TDEXLogSmgrInit();
+
+		principalKey = GetPrincipalKey(GLOBAL_DATA_TDE_OID, NULL);
+		if (!principalKey)
+		{
+			pg_log_error("could not find server principal key");
+			pg_log_error_hint("Copy PGDATA/pg_tde from the source to the backup destination dir.");
+			exit(1);
+		}
+		pg_tde_save_server_key(principalKey, false);
+		TDEXLogSmgrInitWrite(true);
+	}
+#endif
 
 	/* Temporary replication slots are only supported in 10 and newer */
 	if (PQserverVersion(conn) < MINIMUM_VERSION_FOR_TEMP_SLOTS)
@@ -770,6 +807,14 @@ verify_dir_is_empty_or_create(char *dirname, bool *created, bool *found)
 		case 3:
 		case 4:
 
+#ifdef PERCONA_EXT
+			/* 
+			 * `pg_tde` may exists and contain keys and providers for the WAL
+			 * encryption
+			 */
+			if (strcmp(dirname, PG_TDE_DATA_DIR))
+				return;
+#endif
 			/*
 			 * Exists, not empty
 			 */
@@ -1162,7 +1207,8 @@ CreateBackupStreamer(char *archive_name, char *spclocation,
 			directory = get_tablespace_mapping(spclocation);
 		streamer = bbstreamer_extractor_new(directory,
 											get_tablespace_mapping,
-											progress_update_filename);
+											progress_update_filename,
+											encrypt_wal);
 	}
 	else
 	{
@@ -2201,7 +2247,7 @@ BaseBackup(char *compression_algorithm, char *compression_detail,
 		const char *sqlstate = PQresultErrorField(res, PG_DIAG_SQLSTATE);
 
 		if (sqlstate &&
-			strcmp(sqlstate, ERRCODE_DATA_CORRUPTED) == 0)
+			strcmp(sqlstate, ERRCODE_DATA_CORRUPTED_BCP) == 0)
 		{
 			pg_log_error("checksum error occurred");
 			checksum_failure = true;
@@ -2368,6 +2414,9 @@ main(int argc, char **argv)
 		{"target", required_argument, NULL, 't'},
 		{"tablespace-mapping", required_argument, NULL, 'T'},
 		{"wal-method", required_argument, NULL, 'X'},
+#ifdef PERCONA_EXT
+		{"encrypt-wal", no_argument, NULL, 'E'},
+#endif
 		{"gzip", no_argument, NULL, 'z'},
 		{"compress", required_argument, NULL, 'Z'},
 		{"label", required_argument, NULL, 'l'},
@@ -2422,7 +2471,7 @@ main(int argc, char **argv)
 
 	atexit(cleanup_directories_atexit);
 
-	while ((c = getopt_long(argc, argv, "c:Cd:D:F:h:i:l:nNp:Pr:Rs:S:t:T:U:vwWX:zZ:",
+	while ((c = getopt_long(argc, argv, "c:Cd:D:EF:h:i:l:nNp:Pr:Rs:S:t:T:U:vwWX:zZ:",
 							long_options, &option_index)) != -1)
 	{
 		switch (c)
@@ -2535,6 +2584,11 @@ main(int argc, char **argv)
 					pg_fatal("invalid wal-method option \"%s\", must be \"fetch\", \"stream\", or \"none\"",
 							 optarg);
 				break;
+#ifdef PERCONA_EXT
+			case 'E':
+				encrypt_wal = true;
+				break;
+#endif
 			case 'z':
 				compression_algorithm = "gzip";
 				compression_detail = NULL;
@@ -2711,6 +2765,26 @@ main(int argc, char **argv)
 		pg_log_error("replication slots can only be used with WAL streaming");
 		pg_log_error_hint("Try \"%s --help\" for more information.", progname);
 		exit(1);
+	}
+
+	/*
+	 * Sanity checks for WAL encryption.
+	 */
+	if (encrypt_wal)
+	{
+		if (includewal != STREAM_WAL)
+		{
+			pg_log_error("WAL encryption can only be used with WAL streaming");
+			pg_log_error_hint("Use -X stream with -E.");
+			exit(1);
+		}
+
+		if (format != 'p')
+		{
+			pg_log_error("can not encrypt WAL in tar mode");
+			pg_log_error_hint("Use -Fp with -E.");
+			exit(1);
+		}
 	}
 
 	/*
--- fetools/pg17/pg_basebackup/receivelog.c
+++ fetools/pg17/pg_basebackup/receivelog.c
@@ -25,6 +25,13 @@
 #include "receivelog.h"
 #include "streamutil.h"
 
+#ifdef PERCONA_EXT
+#include "access/pg_tde_fe_init.h"
+#include "access/pg_tde_xlog_smgr.h"
+#include "access/xlog_smgr.h"
+#include "catalog/tde_global_space.h"
+#endif
+
 /* currently open WAL file */
 static Walfile *walfile = NULL;
 static bool reportFlushPosition = false;
@@ -1044,6 +1051,7 @@ ProcessXLogDataMsg(PGconn *conn, StreamCtl *stream, char *copybuf, int len,
 	int			bytes_left;
 	int			bytes_written;
 	int			hdr_len;
+	XLogSegNo 	segno;
 
 	/*
 	 * Once we've decided we don't want to receive any more, just ignore any
@@ -1070,6 +1078,8 @@ ProcessXLogDataMsg(PGconn *conn, StreamCtl *stream, char *copybuf, int len,
 
 	/* Extract WAL location for this block */
 	xlogoff = XLogSegmentOffset(*blockpos, WalSegSz);
+
+	XLByteToSeg(*blockpos, segno, WalSegSz);
 
 	/*
 	 * Verify that the initial location in the stream matches where we think
@@ -1120,6 +1130,19 @@ ProcessXLogDataMsg(PGconn *conn, StreamCtl *stream, char *copybuf, int len,
 				return false;
 			}
 		}
+
+#ifdef PERCONA_EXT
+		if (stream->encrypt)
+		{
+			void* enc_buf = copybuf + hdr_len + bytes_written;
+			WalLocation loc = {.tli = stream->timeline};
+
+			XLogSegNoOffsetToRecPtr(segno, xlogoff, WalSegSz, loc.lsn);
+			tde_ensure_xlog_key_location(loc);
+			TDEXLogCryptBuffer(enc_buf, enc_buf, bytes_to_write,
+							   xlogoff, stream->timeline, segno, WalSegSz);
+		}
+#endif
 
 		if (stream->walmethod->ops->write(walfile,
 										  copybuf + hdr_len + bytes_written,
