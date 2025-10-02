911c0264159e205266def5032e1fac4a6918b7fe
Anders Åstrand <anders.astrand@percona.com>
2025-10-03 00:14:22 +0200

    Fix sanitizers CI workflow
    To work properly after the repo split. Also only run the pg_tde tests
    since testing PSP is not the job of this repo.

--- ci_scripts/make-test.sh
+++ ci_scripts/make-test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 set -e
-
 SCRIPT_DIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 
 cd $SCRIPT_DIR/..
 
 ../pginst/bin/pg_ctl -D regress_install -l regress_install.log init -o '--set shared_preload_libraries=pg_tde'
+
+if [ "$1" = "sanitize" ]; then
+	echo 'max_stack_depth=8MB' >> regress_install/postgresql.conf
+fi
+
 ../pginst/bin/pg_ctl -D regress_install -l regress_install.log start
 
 make PG_CONFIG="../pginst/bin/pg_config" installcheck
