1dfe88406adb7ce822e182edae5d6c0fc7ced1b8
Andreas Karlsson <andreas.karlsson@percona.com>
2025-11-04 12:50:09 +0100

    Remove "make-" prefix form CI scripts
    Since we do not plan to support more than one build system, which right
    now is make but could be Meson in the future let's remove the prefixes
    from the scripts called by our CI.

--- ci_scripts/dump-typedefs.sh
+++ ci_scripts/dump-typedefs.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
 if ! test -f pg_tde.so; then
-  echo "pg_tde.so doesn't exists, run make-build.sh first in debug mode"
+  echo "pg_tde.so doesn't exists, run build.sh first in debug mode"
   exit 1
 fi
 
