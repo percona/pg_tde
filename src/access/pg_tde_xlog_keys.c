5fb6d73b456219aaae8e86afe6401ab11e4b0899
Andrew Pogrebnoy <absourd.noise@gmail.com>
2025-11-06 12:53:18 +0200

    PG-1968 Add support for AES 256
    Adds a respective GUC and enables TDE AES lib to handle 128 and 256-bit keys.
    Also adds the support for 256 bit encryption of primary keys

--- src/access/pg_tde_xlog_keys.c
+++ src/access/pg_tde_xlog_keys.c
@@ -45,7 +45,7 @@ typedef struct WalKeyFileEntry
 	uint32		_unused1;		/* Part of AAD, is 1 or 2 in existing entries */
 	uint32		_unused2;		/* Part of AAD */
 
-	uint8		encrypted_key_data[INTERNAL_KEY_LEN];
+	uint8		encrypted_key_data[INTERNAL_KEY_MAX_LEN];
 	uint8		key_base_iv[INTERNAL_KEY_IV_LEN];
 
 	uint32		range_type;		/* WalEncryptionRangeType */
@@ -199,7 +199,7 @@ pg_tde_create_wal_range(WalEncryptionRange *range, WalEncryptionRangeType type)
 	range->end.lsn = MaxXLogRecPtr;
 	range->end.tli = MaxTimeLineID;
 
-	pg_tde_generate_internal_key(&range->key);
+	pg_tde_generate_internal_key(&range->key, 16);
 
 	pg_tde_write_wal_key_file_entry(range, principal_key);
 
@@ -617,10 +617,10 @@ pg_tde_wal_range_from_entry(const TDEPrincipalKey *principal_key, WalKeyFileEntr
 	range->end.lsn = MaxXLogRecPtr;
 
 	memcpy(range->key.base_iv, entry->key_base_iv, INTERNAL_KEY_IV_LEN);
-	if (!AesGcmDecrypt(principal_key->keyData,
+	if (!AesGcmDecrypt(principal_key->keyData, 16,
 					   entry->entry_iv, MAP_ENTRY_IV_SIZE,
 					   (unsigned char *) entry, offsetof(WalKeyFileEntry, encrypted_key_data),
-					   entry->encrypted_key_data, INTERNAL_KEY_LEN,
+					   entry->encrypted_key_data, 16,
 					   range->key.key,
 					   entry->aead_tag, MAP_ENTRY_AEAD_TAG_SIZE))
 		ereport(ERROR,
@@ -680,10 +680,10 @@ pg_tde_initialize_wal_key_file_entry(WalKeyFileEntry *entry,
 				errcode(ERRCODE_INTERNAL_ERROR),
 				errmsg("could not generate iv for wal key file entry: %s", ERR_error_string(ERR_get_error(), NULL)));
 
-	AesGcmEncrypt(principal_key->keyData,
+	AesGcmEncrypt(principal_key->keyData, 16,
 				  entry->entry_iv, MAP_ENTRY_IV_SIZE,
 				  (unsigned char *) entry, offsetof(WalKeyFileEntry, encrypted_key_data),
-				  range->key.key, INTERNAL_KEY_LEN,
+				  range->key.key, 16,
 				  entry->encrypted_key_data,
 				  entry->aead_tag, MAP_ENTRY_AEAD_TAG_SIZE);
 }
