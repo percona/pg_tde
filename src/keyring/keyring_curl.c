af223828cf21bc84da573256d9d43bbff76f28ee
Andreas Karlsson <andreas.karlsson@percona.com>
2025-11-17 21:26:07 +0100

    Remove CURLOPT_USE_SSL since it is only for STARTTLS
    This option is not used by http in curl.

--- src/keyring/keyring_curl.c
+++ src/keyring/keyring_curl.c
@@ -43,8 +43,6 @@ curlSetupSession(const char *url, const char *caFile, CurlString *outStr)
 
 	if (curl_easy_setopt(keyringCurl, CURLOPT_SSL_VERIFYPEER, 1) != CURLE_OK)
 		return 0;
-	if (curl_easy_setopt(keyringCurl, CURLOPT_USE_SSL, CURLUSESSL_ALL) != CURLE_OK)
-		return 0;
 	if (caFile != NULL && strlen(caFile) != 0)
 	{
 		if (curl_easy_setopt(keyringCurl, CURLOPT_CAINFO, caFile) != CURLE_OK)
