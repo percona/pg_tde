e8e120e7e364e6d7ef345feb6f8c0b85bbb61143
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2026-01-13 19:38:47 +0100

    PG-2088: Reduce vault mount point into failures to warning
    If we can't access the mount point info, we emit a warning instead of an
    error. This shown users that we weren't able to verify their setup
    without completely blocking the use of the server.

--- src/keyring/keyring_vault.c
+++ src/keyring/keyring_vault.c
@@ -370,31 +370,31 @@ validate(GenericKeyring *keyring)
 	json_error = parse_vault_mount_info(&parse, jlex);
 
 	if (json_error != JSON_SUCCESS)
-		ereport(ERROR,
+		ereport(WARNING,
 				errcode(ERRCODE_INVALID_JSON_TEXT),
 				errmsg("failed to parse mount info for \"%s\" at mountpoint \"%s\": %s",
 					   vault_keyring->vault_url, vault_keyring->vault_mount_path, json_errdetail(json_error, jlex)));
 
-	if (parse.type == NULL)
-		ereport(ERROR,
+	else if (parse.type == NULL)
+		ereport(WARNING,
 				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("failed to parse mount info for \"%s\" at mountpoint \"%s\": missing type field",
 					   vault_keyring->vault_url, vault_keyring->vault_mount_path));
 
-	if (strcmp(parse.type, "kv") != 0)
+	else if (strcmp(parse.type, "kv") != 0)
 		ereport(ERROR,
 				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("vault mount at \"%s\" has unsupported engine type \"%s\"",
 					   vault_keyring->vault_mount_path, parse.type),
 				errhint("The only supported vault engine type is Key/Value version \"2\""));
 
-	if (parse.version == NULL)
+	else if (parse.version == NULL)
 		ereport(ERROR,
 				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("failed to parse mount info for \"%s\" at mountpoint \"%s\": missing version field",
 					   vault_keyring->vault_url, vault_keyring->vault_mount_path));
 
-	if (strcmp(parse.version, "2") != 0)
+	else if (strcmp(parse.version, "2") != 0)
 		ereport(ERROR,
 				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("vault mount at \"%s\" has unsupported Key/Value engine version \"%s\"",
