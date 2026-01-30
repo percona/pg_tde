1e35c6889d6cf8f43f1a0e7b74ac303ed880efe4
Artem Gavrilov <artem.gavrilov@percona.com>
2026-01-30 21:49:49 +0200

    Fix

--- ci_scripts/build-and-install-psp.sh
+++ ci_scripts/build-and-install-psp.sh
@@ -54,7 +54,7 @@ cd "$PSP_DIR"
    --enable-tap-tests \
    $ARGS
 
-make install-world -j
+make install-world
 
 if [ "$INSTALL_INJECTION_POINTS" = 1 ]; then
     # Injection points extension is not built by default
