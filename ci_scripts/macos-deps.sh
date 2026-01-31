b767f375d68f75bc35f855d42a9cedc2903deb24
Artem Gavrilov <artem.gavrilov@percona.com>
2026-01-31 14:03:22 +0200

    Fix

--- ci_scripts/macos-deps.sh
+++ ci_scripts/macos-deps.sh
@@ -35,3 +35,10 @@ wget https://github.com/openbao/openbao/releases/download/v2.4.3/bao_2.4.3_Darwi
 tar -xzf bao_2.4.3_Darwin_arm64.tar.gz
 
 sudo mv bao /usr/local/bin/bao
+
+if [ -v GITHUB_ACTIONS ]; then
+    echo "CPPFLAGS=-I/opt/homebrew/include" >> $GITHUB_ENV
+    echo "LDFLAGS=-L/opt/homebrew/lib" >> $GITHUB_ENV
+    echo "PKG_CONFIG_PATH=/opt/homebrew/opt/icu4c/lib/pkgconfig" >> $GITHUB_ENV
+    echo "XML_CATALOG_FILES=/opt/homebrew/etc/xml/catalog" >> $GITHUB_ENV
+fi
