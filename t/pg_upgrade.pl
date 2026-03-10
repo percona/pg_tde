f6d74d409acc25a392c3eb8c1ddf3f222b104a40
Andreas Karlsson <andreas.karlsson@percona.com>
2026-03-10 20:20:46 +0100

    PG-2240 Add test for pg_upgrade with encrypted WAL
    The test case manually makes sure to run pg_tde_resetwal isntead of
    pg_tdewal. A tool for automating this will be added in a future commit.

--- t/pg_upgrade.pl
+++ t/pg_upgrade.pl
@@ -17,16 +17,26 @@ CREATE EXTENSION pg_tde;
 SELECT pg_tde_add_global_key_provider_file('file-vault', '/tmp/pg_tde_test_pg_upgrade.per');
 SELECT pg_tde_create_key_using_global_key_provider('server-key', 'file-vault');
 SELECT pg_tde_set_key_using_global_key_provider('server-key', 'file-vault');
+SELECT pg_tde_set_server_key_using_global_key_provider('server-key', 'file-vault');
+ALTER SYSTEM SET pg_tde.wal_encrypt = on;
 
 CREATE TABLE test_enc (k int, PRIMARY KEY (k)) USING tde_heap;
 
 INSERT INTO test_enc (k) VALUES (1), (2);
 ");
+$old->restart;
 $old->stop;
 
 my $new = PostgreSQL::Test::Cluster->new('new');
 $new->init;
 $new->append_conf('postgresql.conf', "shared_preload_libraries = 'pg_tde'");
+$new->append_conf('postgresql.conf', "pg_tde.wal_encrypt = on");
+
+# TODO: Automate this with a script?
+my $tempbin = PostgreSQL::Test::Utils::tempdir;
+system_or_bail('cp', '-R', $new->config_data('--bindir') . '/.', $tempbin);
+system_or_bail('cp', $tempbin . '/pg_tde_resetwal',
+	$tempbin . '/pg_resetwal');
 
 system_or_bail(
 	'cp', '-R',
@@ -39,7 +49,7 @@ command_ok(
 		'--old-datadir' => $old->data_dir,
 		'--new-datadir' => $new->data_dir,
 		'--old-bindir' => $old->config_data('--bindir'),
-		'--new-bindir' => $new->config_data('--bindir'),
+		'--new-bindir' => $tempbin,
 		'--socketdir' => $new->host,
 		'--old-port' => $old->port,
 		'--new-port' => $new->port,
