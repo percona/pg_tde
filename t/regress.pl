0f8803667fbc79aabf3dba2ffd03a937cbfd69ca
Andreas Karlsson <andreas.karlsson@percona.com>
2026-04-02 21:51:35 +0200

    Run OpenBao automatically in TAP tests
    To make running the tests easier we let the TAP tests handle the
    statrting, stopping and configuring of OpenBao. The plan is to do the
    same for OpenKMIP but let's do things step by step.
    
    In the future we may want to add the ability to run the test suite
    against e.g. Hashicorp Vault Enterprise. For that we could for example
    add an environment variable which tells the tests to connect an existing
    server rather than spawning a new one. But let's cross that bridge when
    we get to it.

--- t/regress.pl
+++ t/regress.pl
@@ -1,10 +1,14 @@
 use strict;
 use warnings FATAL => 'all';
 
+use Carp;
+use JSON::XS;
 use PostgreSQL::Test::Cluster;
+use PostgreSQL::Test::Utils;
 
 my $orig_stdout;
 my $orig_stderr;
+my $bao_pid;
 
 BEGIN
 {
@@ -39,6 +43,8 @@ $node->init;
 $node->append_conf('postgresql.conf', "shared_preload_libraries = 'pg_tde'");
 $node->start;
 
+bao_setup();
+
 IPC::Run::run [
 	$ENV{PG_REGRESS},
 	'--host' => $node->host,
@@ -51,3 +57,76 @@ IPC::Run::run [
   or exit $?;
 
 $node->stop;
+
+sub bao_setup
+{
+	my $bao_temp = PostgreSQL::Test::Utils::tempdir('bao');
+	my $bao_port = PostgreSQL::Test::Cluster::get_free_port();
+
+	$bao_pid = fork;
+	unless ($bao_pid)
+	{
+		exec('bao', 'server', '-dev', '-dev-tls',
+			"-dev-cluster-json=$bao_temp/info");
+	}
+
+	wait_for_file("$bao_temp/info", '.');
+
+	my $bao_info = decode_json(slurp_file("$bao_temp/info"));
+
+	$ENV{'VAULT_CACERT'} = $bao_info->{ca_cert_path};
+
+	# We need to enable key/value version 1 engine for just for tests
+	system_or_bail('bao', 'secrets', 'enable', '-path=kv-v1', '-version=1',
+		'kv');
+
+	# Create a test namespace for the tests to test namespace support
+	system_or_bail('bao', 'namespace', 'create', 'pgns');
+	system_or_bail('bao', 'secrets', 'enable', '-ns=pgns', '-path=secret',
+		'-description="Production Secrets"', 'kv-v2');
+
+	write_file("$bao_temp/token", $bao_info->{root_token});
+
+	$ENV{'VAULT_ROOT_TOKEN_FILE'} = "$bao_temp/token";
+}
+
+sub write_file
+{
+	my ($filename, $str) = @_;
+	open my $fh, ">", $filename
+	  or croak "could not write \"$filename\": $!";
+	print $fh $str;
+	close $fh;
+	return;
+}
+
+# Taken from PostgreSQL 19
+sub wait_for_file
+{
+	my ($filename, $regexp, $offset) = @_;
+	$offset = 0 unless defined $offset;
+
+	my $max_attempts = 10 * $PostgreSQL::Test::Utils::timeout_default;
+	my $attempts = 0;
+
+	while ($attempts < $max_attempts)
+	{
+		if (-e $filename)
+		{
+			my $contents = slurp_file($filename, $offset);
+			return $offset + length($contents) if ($contents =~ m/$regexp/);
+		}
+
+		# Wait 0.1 second before retrying.
+		Time::HiRes::usleep(100_000);
+
+		$attempts++;
+	}
+
+	croak "timed out waiting for file $filename contents to match: $regexp";
+}
+
+END
+{
+	kill('TERM', $bao_pid) if $bao_pid;
+}
