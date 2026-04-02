21f3056a7c6d0c443f5e6a2b8c29a840deec722d
Andreas Karlsson <andreas.karlsson@percona.com>
2026-04-02 21:51:35 +0200

    Run OpenBao automatically in TAP tests
    To make running the tests easier we let the TAP tests handle the
    statrting, stopping and configuring of OpenBao. The plan is to do the
    same for OpenKMIP but let's do things step by step.
    
    In the future we may want to add the ability to run the test suite
    against e.g. Hashicorp Vault Enterprise. For that we could for example
    add an environment variable which tells the tests to connect an existing
    server rather than spawning a new one. But let's cross that bridge when
    we get to it.

--- ci_scripts/setup-keyring-servers.sh
+++ ci_scripts/setup-keyring-servers.sh
@@ -17,23 +17,3 @@ cd ..
 echo $SCRIPT_DIR
 rm -f /tmp/pykmip.db
 pykmip-server -f "$SCRIPT_DIR/../pykmip-server.conf" -l /tmp/kmip-server.log &
-
-CLUSTER_INFO=$(mktemp)
-bao server -dev -dev-tls -dev-cluster-json="$CLUSTER_INFO" > /dev/null &
-sleep 10
-export VAULT_ROOT_TOKEN_FILE=$(mktemp)
-jq -r .root_token "$CLUSTER_INFO" > "$VAULT_ROOT_TOKEN_FILE"
-export VAULT_CACERT=$(jq -r .ca_cert_path "$CLUSTER_INFO")
-rm "$CLUSTER_INFO"
-
-## We need to enable key/value version 1 engine for just for tests
-bao secrets enable -path=kv-v1 -version=1 kv
-
-## Create a test namespace for the tests to test namespace support
-bao namespace create pgns
-bao secrets enable -ns=pgns -path=secret -description="Production Secrets" kv-v2
-
-if [[ -n $GITHUB_ACTIONS ]]; then
-    echo "VAULT_ROOT_TOKEN_FILE=$VAULT_ROOT_TOKEN_FILE" >> $GITHUB_ENV
-    echo "VAULT_CACERT=$VAULT_CACERT" >> $GITHUB_ENV
-fi
