35c105b8acd3ce99c149c3bd22b4a3210152eede
Andreas Karlsson <andreas.karlsson@percona.com>
2026-05-26 20:44:51 +0200

    Fix replication.pl

--- t/replication.pl
+++ t/replication.pl
@@ -2,14 +2,15 @@
 
 use strict;
 use warnings;
-use File::Basename;
+use PostgreSQL::Test::Cluster;
+use PostgreSQL::Test::Utils;
 use Test::More;
 use lib 't';
 use pgtde;
 
-PGTDE::setup_files_dir(basename($0));
+my $stdout;
 
-unlink('/tmp/replication.per');
+my $keydir = PostgreSQL::Test::Utils::tempdir;
 
 my $primary = PostgreSQL::Test::Cluster->new('primary');
 $primary->init(allows_streaming => 1);
@@ -26,78 +27,67 @@ $replica->init_from_backup($primary, 'backup', has_streaming => 1);
 $replica->set_standby_mode();
 $replica->start;
 
-PGTDE::append_to_result_file("-- At primary");
+$primary->safe_psql(
+	'postgres', qq(
+	CREATE EXTENSION pg_tde;
+	SELECT pg_tde_add_database_key_provider_file('file-vault', '$keydir/db.keys');
+	SELECT pg_tde_create_key_using_database_key_provider('test-key', 'file-vault');
+	SELECT pg_tde_set_key_using_database_key_provider('test-key', 'file-vault');
 
-PGTDE::psql($primary, 'postgres', 'CREATE EXTENSION pg_tde;');
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_add_database_key_provider_file('file-vault', '/tmp/replication.per');"
-);
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_create_key_using_database_key_provider('test-key', 'file-vault');"
-);
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_set_key_using_database_key_provider('test-key', 'file-vault');"
-);
+	CREATE TABLE test_enc (x int PRIMARY KEY) USING tde_heap;
+	INSERT INTO test_enc (x) VALUES (1), (2);
 
-PGTDE::psql($primary, 'postgres',
-	"CREATE TABLE test_enc (x int PRIMARY KEY) USING tde_heap;");
-PGTDE::psql($primary, 'postgres',
-	"INSERT INTO test_enc (x) VALUES (1), (2);");
-
-PGTDE::psql($primary, 'postgres',
-	"CREATE TABLE test_plain (x int PRIMARY KEY) USING heap;");
-PGTDE::psql($primary, 'postgres',
-	"INSERT INTO test_plain (x) VALUES (3), (4);");
+	CREATE TABLE test_plain (x int PRIMARY KEY) USING heap;
+	INSERT INTO test_plain (x) VALUES (3), (4);
+));
 
 $primary->wait_for_catchup('replica');
 
-PGTDE::append_to_result_file("-- At replica");
-
-PGTDE::psql($replica, 'postgres', "SELECT pg_tde_is_encrypted('test_enc');");
-PGTDE::psql($replica, 'postgres',
+$stdout =
+  $replica->safe_psql('postgres', "SELECT pg_tde_is_encrypted('test_enc');");
+is($stdout, 't', 'test_enc is encrypted');
+$stdout = $replica->safe_psql('postgres',
 	"SELECT pg_tde_is_encrypted('test_enc_pkey');");
-PGTDE::psql($replica, 'postgres', "SELECT * FROM test_enc ORDER BY x;");
+is($stdout, 't', 'test_enc_pkey is encrypted');
+$stdout =
+  $replica->safe_psql('postgres', 'SELECT * FROM test_enc ORDER BY x;');
+is($stdout, "1\n2", 'can read from test_enc');
 
-PGTDE::psql($replica, 'postgres',
+$stdout = $replica->safe_psql('postgres',
 	"SELECT pg_tde_is_encrypted('test_plain');");
-PGTDE::psql($replica, 'postgres',
+is($stdout, 'f', 'test_plain is not encrypted');
+$stdout = $replica->safe_psql('postgres',
 	"SELECT pg_tde_is_encrypted('test_plain_pkey');");
-PGTDE::psql($replica, 'postgres', "SELECT * FROM test_plain ORDER BY x;");
+is($stdout, 'f', 'test_plain_pkey is not encrypted');
+$stdout =
+  $replica->safe_psql('postgres', 'SELECT * FROM test_plain ORDER BY x;');
+is($stdout, "3\n4", 'can read from test_plain');
 
-PGTDE::append_to_result_file("-- check primary crash with WAL encryption");
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_add_global_key_provider_file('file-vault', '/tmp/replication.per');"
-);
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_create_key_using_global_key_provider('test-global-key', 'file-vault');"
-);
-PGTDE::psql($primary, 'postgres',
-	"SELECT pg_tde_set_server_key_using_global_key_provider('test-global-key', 'file-vault');"
-);
+# check primary crash with WAL encryption
+#
+# TODO: This does not seem to actually test WAL encryption
+$primary->safe_psql(
+	'postgres', qq(
+	SELECT pg_tde_add_global_key_provider_file('file-vault', '$keydir/global.key');
+	SELECT pg_tde_create_key_using_global_key_provider('test-global-key', 'file-vault');
+	SELECT pg_tde_set_server_key_using_global_key_provider('test-global-key', 'file-vault');
 
-PGTDE::psql($primary, 'postgres',
-	"CREATE TABLE test_enc2 (x int PRIMARY KEY) USING tde_heap;");
-PGTDE::psql($primary, 'postgres',
-	"INSERT INTO test_enc2 (x) VALUES (1), (2);");
+	CREATE TABLE test_enc2 (x int PRIMARY KEY) USING tde_heap;
+	INSERT INTO test_enc2 (x) VALUES (1), (2);
 
-PGTDE::psql($primary, 'postgres',
-	"ALTER SYSTEM SET pg_tde.wal_encrypt = 'on';");
+	ALTER SYSTEM SET pg_tde.wal_encrypt = 'on';
+));
+
 $primary->kill9;
 
-PGTDE::append_to_result_file("-- primary start");
 PGTDE::poll_start($primary);
 $primary->wait_for_catchup('replica');
 
-PGTDE::psql($replica, 'postgres', "SELECT * FROM test_enc2 ORDER BY x;");
+$stdout =
+  $replica->safe_psql('postgres', 'SELECT * FROM test_enc2 ORDER BY x;');
+is($stdout, "1\n2", 'can read from test_enc2');
 
 $replica->stop;
 $primary->stop;
-
-# Compare the expected and out file
-my $compare = PGTDE->compare_results();
-
-is($compare, 0,
-	"Compare Files: $PGTDE::expected_filename_with_path and $PGTDE::out_filename_with_path files."
-);
 
 done_testing();
